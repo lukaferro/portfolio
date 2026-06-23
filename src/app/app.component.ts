cd94460e4f5f136d382606f0fb330e11858413a7
Luca Ferro <luca.ferro2003@gmail.com>
2026-06-23 19:09:47 +0200

    chore: commit root component, config and global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ParticlesComponent } from './components/particles/particles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ParticlesComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent { }
