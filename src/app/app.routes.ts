11879e9563400ca530f3935c110ff28082765c39
Luca Ferro <luca.ferro2003@gmail.com>
2026-06-23 18:14:25 +0200

    feat: add custom 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
-
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'studi', loadComponent: () => import('./pages/studi/studi.component').then(c => c.StudiComponent) },
+    { path: 'esperienze', loadComponent: () => import('./pages/esperienze/esperienze.component').then(c => c.EsperienzeComponent) },
+    { path: 'contatti', loadComponent: () => import('./pages/contatti/contatti.component').then(c => c.ContattiComponent) },
+    { path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent) }
 ];
