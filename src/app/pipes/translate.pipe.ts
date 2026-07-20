69dcc541238408569e8797f57db28f1b77b4eebd
Luca Ferro <luca.ferro2003@gmail.com>
2026-07-20 19:17:22 +0200

    fix: pipe translate impure per lingua reattiva, CV apre in viewer

--- src/app/pipes/translate.pipe.ts
+++ src/app/pipes/translate.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform, inject } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
 
-@Pipe({ name: 'translate', standalone: true })
+@Pipe({ name: 'translate', standalone: true, pure: false })
 export class TranslatePipe implements PipeTransform {
   private ts = inject(TranslationService);
 
